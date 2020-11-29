cf237fe3d8e96084e07aff5c579da3d0ea648b2c
Smith <smithwebdd@gmail.com>
2020-11-28 21:09:36 -0500

    Implemented state for New Recipe creation

--- app/javascript/packs/routes/index.routes.jsx
+++ app/javascript/packs/routes/index.routes.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/home/home.component';
 import Recipes from '../components/recipes/recipes.component';
 import Recipe from '../components/recipes/recipe.component';
+import NewRecipe from '../components/recipes/newrecipe.component';
+
 
 
 
@@ -13,6 +15,8 @@ export default (
       <Route exact path="/" component={Home} />
       <Route exact path='/recipes' component={ Recipes }/>
       <Route exact path="/recipe/:id" component={ Recipe }/>
+      <Route exact path='/recipe' component={ NewRecipe }/>
+      
     </Switch>
   </Router>
 );
