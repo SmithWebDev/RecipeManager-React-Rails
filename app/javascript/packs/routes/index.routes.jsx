e7051915f5d00a604fc6c4ed1c0cf41631484093
Smith <smithwebdd@gmail.com>
2020-11-28 19:19:13 -0500

    Implemented states for viewing all recipes and individual recipes

--- app/javascript/packs/routes/index.routes.jsx
+++ app/javascript/packs/routes/index.routes.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/home/home.component';
+import Recipes from '../components/recipes/recipes.component';
+import Recipe from '../components/recipes/recipe.component';
+
+
 
 
 export default (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path='/recipes' component={ Recipes }/>
+      <Route exact path="/recipe/:id" component={ Recipe }/>
     </Switch>
   </Router>
 );
